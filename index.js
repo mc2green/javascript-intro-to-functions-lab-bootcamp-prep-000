11349f8227cc356a4a7ff9087501c355d25a6c1e
Matthew Chen <mc.2green@gmail.com>
2019-07-16 21:02:03 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -21,7 +21,9 @@ function sayHiToGrandma(string){
   if (string.toUpperCase === string){
     return "YES INDEED!"
   }
-  if ()
+  if (string === "I love you, Grandma."){
+    return "I love you , too"
+  }
 
   
 }
