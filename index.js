8feea27c356008df131d5234091d0b652d4318e5
Matthew Chen <mc.2green@gmail.com>
2019-07-16 20:59:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -15,6 +15,9 @@ function logWhisper(string){
 }
 
 function sayHiToGrandma(string){
-  if (string.toLowerCase === string)
-  return 
+  if (string.toLowerCase === string){
+    return "I can't hear you!"
+  }
+
+  
 }
